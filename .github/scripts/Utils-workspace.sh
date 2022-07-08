47fc0d51bb0aca31b4461359b2db8022ef3e7368
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-09 00:12:16 +0100

    config resuable template structure

--- .github/scripts/Utils-workspace.sh
+++ .github/scripts/Utils-workspace.sh
@@ -2,7 +2,7 @@ echo "environment variable: $test_TemplateParamFilePath"
 echo "environment variable: $test_Location"
 echo "environment variable: $test_TemplateFilePath"
 
-az deployment sub create \
---location $test_Location \
---template-file $test_TemplateFilePath \
---parameters $test_TemplateParamFilePath
+#az deployment sub create \
+#--location $test_Location \
+#--template-file $test_TemplateFilePath \
+#--parameters $test_TemplateParamFilePath
