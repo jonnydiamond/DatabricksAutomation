5f77ccccc26ec08db9114a849eab07ad5bf64f94
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-13 18:57:31 +0100

    config resuable template structure

--- .github/workflows/Utilities/Utils-Assign-RBAC.sh
+++ .github/workflows/Utilities/Utils-Assign-RBAC.sh
@@ -4,7 +4,8 @@ echo $COUNTER
 
 echo "param_RBAC_Assignments_"$COUNTER"_role"
 
-echo ${"param_RBAC_Assignments_"$COUNTER"_role"}
+echo ${ "param_RBAC_Assignments_"$COUNTER"_role" }
+
 echo "environment variable: $param_resourceGroupName"
 
 #while [ -z "${param_RBAC_Assignments_$COUNTER_role}"]
