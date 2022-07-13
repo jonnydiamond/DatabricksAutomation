88e62a0515fc5e433ebda768210ea477aa967c1d
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-13 22:35:09 +0100

    config resuable template structure

--- .github/workflows/Utilities/Utils-Assign-RBAC.sh
+++ .github/workflows/Utilities/Utils-Assign-RBAC.sh
@@ -4,7 +4,7 @@ i=1
 echo $i
 
 
-test=$( jq '[.RBAC_Assignments[]]' .github/workflows/Global_Parameters/Development.json)
+test=$( jq -r '.RBAC_Assignments[]' .github/workflows/Global_Parameters/Development.json)
 echo $test
 
 
