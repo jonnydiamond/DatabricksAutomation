ae1ce1673080b3bfec0dd84b556d6cdf629d6fc8
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-15 15:23:53 +0100

    config resuable template structure

--- .github/workflows/Utilities/Utils-Create-PAToken.sh
+++ .github/workflows/Utilities/Utils-Create-PAToken.sh
@@ -1,4 +1,9 @@
 az config set extension.use_dynamic_install=yes_without_prompt
+
+echo $ARM_CLIENT_ID
+echo $ARM_CLIENT_SECRET
+echo $ARM_TENANT_ID
+
 az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
 dbx_workspace_name=$(az databricks workspace list \
