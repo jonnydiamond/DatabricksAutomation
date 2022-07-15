d98455720afb7aae393e8e3f186b51a74bdd72a0
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-15 13:42:30 +0100

    config resuable template structure

--- .github/workflows/Utilities/Utils-Assign-RBAC.sh
+++ .github/workflows/Utilities/Utils-Assign-RBAC.sh
@@ -4,16 +4,19 @@
 
 echo "Ingest and Format JSON File"
 json=$( jq '.' .github/workflows/Global_Parameters/Development.json)
+echo $json
 
 echo "Iterate And Assign RBAC Permissions"
 for row in $(echo "${json}" | jq -r '.RBAC_Assignments[] | @base64'); do
     _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
     }
-    
+
     az role assignment create \
     --role "$(_jq '.role')" \
     --assignee-object-id $(_jq '.roleBeneficiaryObjID') \
     --assignee-principal-type "ServicePrincipal" \
     --scope "$(_jq '.scope')"
+done
+
 
