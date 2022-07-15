fc12680467816a92cd8f09118f90671299def4e8
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-15 13:49:37 +0100

    config resuable template structure

--- .github/workflows/Utilities/Utils-Assign-RBAC.sh
+++ .github/workflows/Utilities/Utils-Assign-RBAC.sh
@@ -2,9 +2,9 @@
 
 # Ensure That Your DevOps/PipelineAgent Has Owner RBAC Assigned. Do This Manually In Azure Portal 
 
-echo "Ingest and Format JSON File"
+echo "Ingest and JSON File"
 json=$( jq '.' .github/workflows/Global_Parameters/Development.json)
-echo $json
+echo "${json}" | jq
 
 echo "Iterate And Assign RBAC Permissions"
 for row in $(echo "${json}" | jq -r '.RBAC_Assignments[] | @base64'); do
