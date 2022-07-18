1d77b861560b580a75f8b696575e7c10cd168fa7
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-18 20:35:30 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Create-Cluster.sh
+++ .github/workflows/Utilities/Utils-Create-Cluster.sh
@@ -7,6 +7,11 @@
 #---------------------------------------------------
 echo $environment
 
+az config set extension.use_dynamic_install=yes_without_prompt
+dbx_workspace_name=$(az databricks workspace list -g $param_ResourceGroupName --query "[].name" -o tsv)
+workspaceUrl=$(az databricks workspace list -g $param_ResourceGroupName --query "[].workspaceUrl" -o tsv)
+workspace_id=$(az databricks workspace list -g $param_ResourceGroupName --query "[].id" -o tsv)
+
 echo "Ingest JSON File"
 json=$( jq '.' .github/workflows/Global_Parameters/$environment.json)
 echo "${json}" | jq
@@ -21,7 +26,7 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     echo "Databricks API: Create Clusters"
     clusterscreate=$(curl -X POST -H "Authorization: Bearer $token" \
     -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" \
+    -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
     -H 'Content-Type: application/json' -d \
     '{
         "cluster_name": "$(_jq '.cluster_name')", 
