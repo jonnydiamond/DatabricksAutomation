00f27ec75189b237ca3821bf510bfade8add9082
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-18 16:00:23 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-DBX-SP-Authenticate.sh
+++ .github/workflows/Utilities/Utils-DBX-SP-Authenticate.sh
@@ -19,20 +19,14 @@ echo "Workspace URL Set As Env Variable: $workspaceUrl"
 
 # token response for the azure databricks app  
 token_response=$(az account get-access-token --resource $param_AZURE_DATABRICKS_APP_ID)
-echo "Token response: $token_response"
-echo "Management Access Set As Env Variable: $Management_Access_Token"
-
-# Extract accessToken value --> We use 'Here Strings to turn the JSON token response into an Array, and then extract the accessToken'
 token=$(jq .accessToken -r <<< "$token_response")
-echo "Token: $token"
-echo "Token=$token" >> $GITHUB_ENV
-echo "Token Set As Env Variable: $Token"
+echo "token=$token" >> $GITHUB_ENV
+echo "Token Set As Env Variable: $token"
 
 az_mgmt_resource_endpoint=$(curl -X GET -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&client_id='$ARM_CLIENT_ID'&resource='$param_MANAGEMENT_RESOURCE_ENDPOINT'&client_secret='$ARM_CLIENT_SECRET https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token)
 echo "Management Resource Endpoint: $az_mgmt_resource_endpoint"
 
 # Extract the access_token value
 mgmt_access_token=$(jq .access_token -r <<< "$az_mgmt_resource_endpoint" )
-echo "Management Access Token: $mgmt_access_token"
-echo "Management_Access_Token=$mgmt_access_token" >> $GITHUB_ENV
-echo "Management Access Set As Env Variable: $Management_Access_Token"
+echo "mgmt_access_token=$mgmt_access_token" >> $GITHUB_ENV
+echo "Management Access Set As Env Variable: $mgmt_access_token"
