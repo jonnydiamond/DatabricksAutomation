bd5bba85154a6f6bc503ecc5d9472ef0732c232a
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-18 21:16:45 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Create-Cluster.sh
+++ .github/workflows/Utilities/Utils-Create-Cluster.sh
@@ -22,24 +22,28 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
     }
+    JSON_STRING=$( jq -n -c \
+                --arg cn "$(_jq '.cluster_name')" \
+                --arg sv "$(_jq '.spark_version')" \
+                --arg nt "$(_jq '.node_type_id')"  \
+                --arg nw "$(_jq '.autoscale.max_workers')" \
+                --arg sc "$(_jq '.spark_conf')" \
+                --arg at "$(_jq '.autotermination_minutes')" \
+                '{cluster_name: $cn,
+                spark_version: $sv,
+                node_type_id: $nt,
+                num_workers: ($nw|tonumber),
+                autotermination_minutes: ($at|tonumber),
+                spark_conf: ($sc|fromjson)}' )
+
 
     echo "Databricks API: Create Clusters"
     clusterscreate=$(curl -X POST -H "Authorization: Bearer $token" \
     -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
     -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
-    -H 'Content-Type: application/json' -d \
-    '{
-        "cluster_name": $(_jq '.cluster_name'), 
-        "spark_version": "10.4.x-scala2.12", 
-        "node_type_id": "Standard_D3_v2",
-        "spark_conf": {},
-        "autotermination_minutes": 20,
-        "runtime_engine": "STANDARD",
-        "autoscale": {
-            "min_workers": 2,
-            "max_workers": 8
-        }
-    }' https://$workspaceUrl/api/2.0/clusters/create )
+    -H 'Content-Type: application/json' \
+    -d $JSON_STRING \
+    https://$workspaceUrl/api/2.0/clusters/create )
     
 
     echo "$(_jq '.cluster_name')"
