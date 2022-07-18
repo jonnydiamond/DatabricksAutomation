2b5e7c85c41d7a47a17d6c86172ad3ba5f6304b2
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-18 20:13:06 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Create-Cluster.sh
+++ .github/workflows/Utilities/Utils-Create-Cluster.sh
@@ -1,6 +1,11 @@
-echo "Databricks API: List Clusters"
-clusterslist=$(curl -X GET -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" https://$workspaceUrl/api/2.0/clusters/list)
-echo $clusterslist
+az config set extension.use_dynamic_install=yes_without_prompt
+dbx_workspace_name=$(az databricks workspace list -g $param_ResourceGroupName --query "[].name" -o tsv)
+workspaceUrl=$(az databricks workspace list -g $param_ResourceGroupName --query "[].workspaceUrl" -o tsv)
+workspace_id=$(az databricks workspace list -g $param_ResourceGroupName --query "[].id" -o tsv)
+
+#echo "Databricks API: List Clusters"
+#clusterslist=$(curl -X GET -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" https://$workspaceUrl/api/2.0/clusters/list)
+#echo $clusterslist
 
 #---------------------------------------------------
 echo $environment
@@ -19,7 +24,7 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     echo "Databricks API: Create Clusters"
     clusterscreate=$(curl -X POST -H "Authorization: Bearer $token" \
     -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" \
+    -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
     -H 'Content-Type: application/json' -d \
     '{
         "cluster_name": "$(_jq '.cluster_name')", 
@@ -28,7 +33,10 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
         "spark_conf": "$(_jq '.spark_conf')" ,
         "autotermination_minutes": "$(_jq '.autotermination_minutes')" ,
         "runtime_engine": "$(_jq '.runtime_engine')" ,
-        "autoscale": "$(_jq '.autoscale')" 
+        "autoscale": {
+            "min_workers":"$(_jq '.autoscale.min_workers')",
+            "max_workers":"$(_jq '.autoscale.max_workers')"
+        }
     }' https://$workspaceUrl/api/2.0/clusters/create )
     
 
@@ -37,8 +45,8 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     echo "$(_jq '.node_type_id')" 
     echo "$(_jq '.spark_conf')"
     echo "$(_jq '.autotermination_minutes')"
-    echo "$(_jq '.runtime_engine')"
-    echo "$(_jq '.autoscale')"
+    echo "$(_jq '.runtime_engine.min_workers')"
+    echo "$(_jq '.autoscale.max_workers')"
 
     echo "ClusterCreateOutput"
     echo clusterscreate
