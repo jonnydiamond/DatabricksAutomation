3a4db99711ea33b7f8b3007fe4b17a99cf46effc
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-19 10:03:22 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Create-Cluster.sh
+++ .github/workflows/Utilities/Utils-Create-Cluster.sh
@@ -1,9 +1,6 @@
 #echo "Databricks API: List Clusters"
 #clusterslist=$(curl -X GET -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" https://$workspaceUrl/api/2.0/clusters/list)
 #echo $clusterslist
-
-
-
 #---------------------------------------------------
 echo $environment
 
@@ -17,7 +14,6 @@ json=$( jq '.' .github/workflows/Global_Parameters/$environment.json)
 echo "${json}" | jq
 
 echo "Configure All Clusters From Environment Parameters File"
-
 for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
@@ -36,7 +32,6 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
                 autotermination_minutes: ($at|tonumber),
                 spark_conf: ($sc|fromjson)}' )
 
-
     echo "Databricks API: Create Clusters"
     clusterscreate=$(curl -X POST -H "Authorization: Bearer $token" \
     -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
@@ -44,49 +39,7 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     -H 'Content-Type: application/json' \
     -d $JSON_STRING \
     https://$workspaceUrl/api/2.0/clusters/create )
-    
-
-    echo "$(_jq '.cluster_name')"
-    echo "$(_jq '.spark_version')"
-    echo "$(_jq '.node_type_id')" 
-    echo "$(_jq '.spark_conf')"
-    echo "$(_jq '.autotermination_minutes')"
-    echo "$(_jq '.autoscale.min_workers')"
-    echo "$(_jq '.autoscale.max_workers')"
 
     echo "ClusterCreateOutput"
     echo clusterscreate
 done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#echo "Databricks API: Create Clusters"
-#clusterscreate=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
-#'{
-#    "cluster_name": "dbz-sp-cluster2", 
-#    "spark_version": "10.4.x-scala2.12", 
-#    "node_type_id": "Standard_D3_v2",
-#    "spark_conf": {},
-#    "autotermination_minutes": 120,
-#    "runtime_engine": "STANDARD",
-#    "autoscale": {
-#        "min_workers": 2,
-#        "max_workers": 8
-#    }
-#    }' https://$workspaceUrl/api/2.0/clusters/create )
-#echo $clusterscreate
--- .github/workflows/Utilities/Utils-Repo-Create.sh
+++ .github/workflows/Utilities/Utils-Repo-Create.sh
@@ -1,73 +1,142 @@
-set -o errexit
-set -o nounset
-set -o pipefail
+az config set extension.use_dynamic_install=yes_without_prompt
+dbx_workspace_name=$(az databricks workspace list -g $param_ResourceGroupName --query "[].name" -o tsv)
+workspaceUrl=$(az databricks workspace list -g $param_ResourceGroupName --query "[].workspaceUrl" -o tsv)
+workspace_id=$(az databricks workspace list -g $param_ResourceGroupName --query "[].id" -o tsv)
+
+echo "Ingest JSON File"
+json=$( jq '.' .github/workflows/Global_Parameters/$environment.json)
+echo "${json}" | jq
+
+echo "Configure All Clusters From Environment Parameters File"
+for row in $(echo "${json}" | jq -r '.Git_Configuration[] | @base64'); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+    JSON_STRING=$( jq -n -c \
+                --arg pat "$(_jq '.personal_access_token')" \
+                --arg gu "$(_jq '.git_username')" \
+                --arg gp "$(_jq '.git_provider')"  \
+                '{personal_access_token: $pat,
+                git_username: $gu,
+                git_provider: $gp}' )
+    
+    git_credentials=$(curl -X POST -H "Authorization: Bearer $token" \
+        -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+        -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
+        -H 'Content-Type: application/json' \
+        -d $JSON_STRING \
+        https://$workspaceUrl/api/2.0/git-credentials )
+done
+
+for row in $(echo "${json}" | jq -r '.Repo_Configuration[] | @base64'); do
+    _jq() {
+        echo ${row} | base64 --decode | jq -r ${1}
+    }
+    JSON_STRING=$( jq -n -c \
+                --arg url "$(_jq '.url')" \
+                --arg pr "$(_jq '.provider')" \
+                --arg pa "$(_jq '.path')"  \
+                '{url: $url,
+                provider: $pr,
+                path: $pa}' )
+
+    Repo_Configuration=$(curl -X POST -H "Authorization: Bearer $token" \
+                -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+                -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
+                -H 'Content-Type: application/json' \
+                -d $JSON_STRING \
+                https://$workspaceUrl/api/2.0/repos )
+done
 
 
-echo "Logging in using Azure service principal"
-az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
-
-# az account set -s  $ARM_SUBSCRIPTION_ID
-echo "ClientSecret:                             $ARM_CLIENT_SECRET"
-echo "ClientID:                                 $ARM_CLIENT_ID"
-echo "TenantID:                                 $ARM_TENANT_ID"
-echo "Databricks WS RG:                         $RESOURCE_GROUP"
-echo "workspace id                              $wsId"
-echo "worokspace url                            $workspaceUrl"  
-echo "APP ID                                    $AZURE_DATABRICKS_APP_ID"
 
 
-# token response for the azure databricks app  
-token_response=$(az account get-access-token --resource $AZURE_DATABRICKS_APP_ID)
-echo "Token response: $token_response"
 
-# Extract accessToken value --> We use 'Here Strings to turn the JSON token response into an Array, and then extract the accessToken'
-token=$(jq .accessToken -r <<< "$token_response")
-echo "Token: $token"
 
-az_mgmt_resource_endpoint=$(curl -X GET -H 'Content-Type: application/x-www-form-urlencoded' -d 'grant_type=client_credentials&client_id='$ARM_CLIENT_ID'&resource='$MANAGEMENT_RESOURCE_ENDPOINT'&client_secret='$ARM_CLIENT_SECRET https://login.microsoftonline.com/$ARM_TENANT_ID/oauth2/token)
-echo "Management Resource Endpoint: $az_mgmt_resource_endpoint"
 
-# Extract the access_token value
-mgmt_access_token=$(jq .access_token -r <<< "$az_mgmt_resource_endpoint" )
-echo "Management Access Token: $mgmt_access_token"
 
-git_credentials=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
-'{
-"personal_access_token": "yhdjjk6kcmmv6zcfowmsh6pmcre3jbstwbk5sarxcwtutmlyu5ha", 
-"git_username": "ciaranh@microsoft.com", 
-"git_provider": "azureDevOpsServices"
-}' https://$workspaceUrl/api/2.0/git-credentials )
 
-echo $git_credentials 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#git_credentials=$(curl -X POST -H "Authorization: Bearer $token" \
+#        -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+#        -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
+#        -H 'Content-Type: application/json' \
+#        -d $JSON_STRING
+#        '{
+#        "personal_access_token": "yhdjjk6kcmmv6zcfowmsh6pmcre3jbstwbk5sarxcwtutmlyu5ha", 
+#        "git_username": "ciaranh@microsoft.com", 
+#        "git_provider": "azureDevOpsServices"
+#        }' https://$workspaceUrl/api/2.0/git-credentials )
+
+
+
+
+#git_credentials=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
+#'{
+#"personal_access_token": "yhdjjk6kcmmv6zcfowmsh6pmcre3jbstwbk5sarxcwtutmlyu5ha", 
+#"git_username": "ciaranh@microsoft.com", 
+#"git_provider": "azureDevOpsServices"
+#}' https://$workspaceUrl/api/2.0/git-credentials )
+
+#echo $git_credentials 
 
 ## The Section CREATES The Repos --> 3 Folders 'Production, Staging and Test' in the DBX Repo for the SP. This will be locked Down 
 ## When changes are merged into each branch, an automated pipeline run will update the repo.
 ## This will ensure that the branch for the testing /production repo is up to date when we run jobs on it.
 
 
-create_repo_response=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
-'{
-"url": "https://dev.azure.com/ciaranh0658/_git/devopsTemplates", 
-"provider": "azureDevOpsServices",
-"path": "/Repos/ce79c2ef-170d-4f1c-a706-7814efb94898/Development"
-}' https://$workspaceUrl/api/2.0/repos )
+#create_repo_response=$(curl -X POST -H "Authorization: Bearer $token" \
+#                -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+#                -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
+#                -H 'Content-Type: application/json' -d \
+#'{
+#"url": "https://dev.azure.com/ciaranh0658/_git/devopsTemplates", 
+#"provider": "azureDevOpsServices",
+#"path": "/Repos/ce79c2ef-170d-4f1c-a706-7814efb94898/Development"
+#}' https://$workspaceUrl/api/2.0/repos )
 
-echo $create_repo_response
+#echo $create_repo_response
 
-create_repo_response=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
-'{
-"url": "https://dev.azure.com/ciaranh0658/_git/devopsTemplates", 
-"provider": "azureDevOpsServices",
-"path": "/Repos/ce79c2ef-170d-4f1c-a706-7814efb94898/Production"
-}' https://$workspaceUrl/api/2.0/repos )
+#create_repo_response=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
+#'{
+#"url": "https://dev.azure.com/ciaranh0658/_git/devopsTemplates", 
+#"provider": "azureDevOpsServices",
+#"path": "/Repos/ce79c2ef-170d-4f1c-a706-7814efb94898/Production"
+#}' https://$workspaceUrl/api/2.0/repos )
 
-echo $create_repo_response
+#echo $create_repo_response
 
-create_repo_response=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
-'{
-"url": "https://dev.azure.com/ciaranh0658/_git/devopsTemplates", 
-"provider": "azureDevOpsServices",
-"path": "/Repos/ce79c2ef-170d-4f1c-a706-7814efb94898/Staging"
-}' https://$workspaceUrl/api/2.0/repos )
+#create_repo_response=$(curl -X POST -H "Authorization: Bearer $token" -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" -H "X-Databricks-Azure-Workspace-Resource-Id: $wsId" -H 'Content-Type: application/json' -d \
+#'{
+#"url": "https://dev.azure.com/ciaranh0658/_git/devopsTemplates", 
+#"provider": "azureDevOpsServices",
+#"path": "/Repos/ce79c2ef-170d-4f1c-a706-7814efb94898/Staging"
+#}' https://$workspaceUrl/api/2.0/repos )
 
-echo $create_repo_response
+#echo $create_repo_response
