da4c0fc058331741441b8ab4ab5b759c01deaa07
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-19 10:34:43 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Create-Cluster.sh
+++ .github/workflows/Utilities/Utils-Create-Cluster.sh
@@ -18,6 +18,22 @@ for row in $(echo "${json}" | jq -r '.Clusters[] | @base64'); do
     _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
     }
+
+    listClusters=$(curl -X GET -H "Authorization: Bearer $token" \
+                    -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+                    -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
+                    -H 'Content-Type: application/json' \
+                    https://$workspaceUrl/api/2.0/clusters/list )
+
+    echo "List Clusters"
+    echo $listClusters
+    
+    exit
+
+
+    
+
+
     JSON_STRING=$( jq -n -c \
                 --arg cn "$(_jq '.cluster_name')" \
                 --arg sv "$(_jq '.spark_version')" \
