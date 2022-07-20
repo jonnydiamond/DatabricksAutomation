d6ecaa676315a61eeb18f1413dc11ffc9f52812e
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-20 14:42:10 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Azure-Resources.sh
+++ .github/workflows/Utilities/Utils-Azure-Resources.sh
@@ -6,4 +6,4 @@ az deployment sub create \
 --location $param_Location \
 --template-file $param_TemplateFilePath \
 --parameters $param_TemplateParamFilePath \
---name "Azure_$environment_Resource_Deployment"
+--name "$environment"
