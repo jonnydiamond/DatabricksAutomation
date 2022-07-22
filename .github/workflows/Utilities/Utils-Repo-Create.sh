f067e8ab6b295c001f70a73044504af4ac7d62e3
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-22 12:14:20 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Repo-Create.sh
+++ .github/workflows/Utilities/Utils-Repo-Create.sh
@@ -25,7 +25,7 @@ for row in $(echo "${json}" | jq -r '.Git_Configuration[] | @base64'); do
                 --arg br "$(_jq '.branch')"  \
                 '{personal_access_token: $pat,
                 git_username: $gu,
-                git_provider: $gp
+                git_provider: $gp,
                 branch: $br}' )
     echo $pat
     echo $gu
