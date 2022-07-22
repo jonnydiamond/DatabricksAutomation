2dff80548ba0e8d7590a81997f7f462b38fb55b0
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-22 12:58:07 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-Repo-Update-All.sh
+++ .github/workflows/Utilities/Utils-Repo-Update-All.sh
@@ -7,43 +7,82 @@ workspace_id=$(az databricks workspace list -g $param_ResourceGroupName --query 
 ## The section below updates the repos. We will have it triggered when when the respective branch is updated (successfully merge request)
 
 
-
 reposWithManagePermissions=$(curl -X GET -H "Authorization: Bearer $token" \
                         -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
                         -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
                         -H 'Content-Type: application/json' \
                         https://$workspaceUrl/api/2.0/repos )
 echo $reposWithManagePermissions
-
-
-echo $environment
-
-RepoID=$( jq -r '.repos[] | select( .path | contains($Development)) | .id' <<< "$reposWithManagePermissions")
-echo "Target Branch == Main"
-echo $RepoID
-
-
 json=$( jq '.' .github/workflows/Global_Parameters/$environment.json)
 echo "${json}" | jq
-branch=$( jq -r  ' .Repo_Configuration[]| select( .path | contains("Development"))| .branch ' <<< "$json")
 
 
-JSON_STRING=$( jq -n -c \
+Repo_Folders=(Production Development Staging)
+echo "Repo Folders"
+echo $Repo_Folders
+for Repo_Folder in "${Repo_Folders[@]}" 
+do
+    :
+    
+    RepoID=$( jq -r '.repos[] | select( .path | contains($Repo_Folder)) | .id' <<< "$reposWithManagePermissions")
+    echo "Repo ID"
+    echo $RepoID
+
+
+    branch=$( jq -r  ' .Repo_Configuration[]| select( .path | contains("Repo_Folder"))| .branch ' <<< "$json")
+    echo "Branch"
+    echo $branch
+    JSON_STRING=$( jq -n -c \
                 --arg tb "$branch" \
                 '{branch: $tb}' )
 
-echo $JSON_STRING
-
-# If there is a change to the develop branch, we will update Files in the Staging/Test Folder
-update_repo_response=$(curl -X PATCH \
+    echo $JSON_STRING
+    update_repo_response=$(curl -X PATCH \
         -H "Authorization: Bearer $token" \
         -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
         -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
         -H 'Content-Type: application/json' \
         -d $JSON_STRING \
         https://$workspaceUrl/api/2.0/repos/$RepoID )
+    
+    echo $update_repo_response
+done
 
-echo $update_repo_response
+
+
+
+
+
+
+
+#echo $environment
+
+#RepoID=$( jq -r '.repos[] | select( .path | contains($Development)) | .id' <<< "$reposWithManagePermissions")
+#echo "Target Branch == Main"
+#echo $RepoID
+
+
+#json=$( jq '.' .github/workflows/Global_Parameters/$environment.json)
+#echo "${json}" | jq
+#branch=$( jq -r  ' .Repo_Configuration[]| select( .path | contains("Development"))| .branch ' <<< "$json")
+
+
+#JSON_STRING=$( jq -n -c \
+#                --arg tb "$branch" \
+#                '{branch: $tb}' )
+
+#echo $JSON_STRING
+
+# If there is a change to the develop branch, we will update Files in the Staging/Test Folder
+#update_repo_response=$(curl -X PATCH \
+#        -H "Authorization: Bearer $token" \
+#        -H "X-Databricks-Azure-SP-Management-Token: $mgmt_access_token" \
+#        -H "X-Databricks-Azure-Workspace-Resource-Id: $workspace_id" \
+#        -H 'Content-Type: application/json' \
+#        -d $JSON_STRING \
+#        https://$workspaceUrl/api/2.0/repos/$RepoID )
+
+#echo $update_repo_response
 
 
 
