c4e462dcf6f632d20fde5fd2ff1f875daa0b99d0
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-25 09:18:03 +0100

    config resuable template structure

--- .github/workflows/Utilities/Utils-DBX-SP-Authenticate.sh
+++ .github/workflows/Utilities/Utils-DBX-SP-Authenticate.sh
@@ -7,13 +7,13 @@ echo "Tenant ID: $ARM_TENANT_ID"
 echo "Logging in using Azure service priciple"
 az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
 
-dbx_workspace_name=$(az databricks workspace list -g $param_ResourceGroupName --query "[].workspaceId" -o tsv)
-echo $dbx_workspace_name
-exit 1
-
+dbx_workspace_org_id=$(az databricks workspace list -g $param_ResourceGroupName --query "[].workspaceId" -o tsv)
 dbx_workspace_name=$(az databricks workspace list -g $param_ResourceGroupName --query "[].name" -o tsv)
 workspaceUrl=$(az databricks workspace list -g $param_ResourceGroupName --query "[].workspaceUrl" -o tsv)
 workspace_id=$(az databricks workspace list -g $param_ResourceGroupName --query "[].id" -o tsv)
+
+echo "dbx_workspace_org_id=$dbx_workspace_org_id" >> $GITHUB_ENV
+echo "Workspace ID Set As Env Variable: $dbx_workspace_org_id"
 
 echo "workspace_id=$workspace_id" >> $GITHUB_ENV
 echo "Workspace ID Set As Env Variable: $workspace_id"
