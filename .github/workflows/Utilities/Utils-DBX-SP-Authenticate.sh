03bd4c5aa3a1fcbc0dd8ec5c56fe10c4edc3072f
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-26 14:34:51 +0100

    refactoring code

--- .github/workflows/Utilities/Utils-DBX-SP-Authenticate.sh
+++ .github/workflows/Utilities/Utils-DBX-SP-Authenticate.sh
@@ -24,6 +24,9 @@ echo "Workspace URL Set As Env Variable: $DATABRICKS_INSTANCE"
 echo "DATABRICKS_HOST=https://$DATABRICKS_INSTANCE" >> $GITHUB_ENV
 echo "Workspace URL Set As Env Variable: $DATABRICKS_HOST"
 
+echo "DATABRICKS_TOKEN=$param_DATABRICKS_TOKEN>> $GITHUB_ENV
+echo "Workspace URL Set As Env Variable: $DATABRICKS_TOKEN"
+
 
 echo "Databricks OrgID as Env Variables"
 
