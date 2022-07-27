f8ed3d26c1444556f19c107f2d82733ae670821f
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-27 14:14:32 +0100

    created databricks connect module pip

--- .github/workflows/Utilities/Utils-cli-configure.sh
+++ .github/workflows/Utilities/Utils-cli-configure.sh
@@ -1,5 +1,5 @@
 echo $DATABRICKS_HOST
-echo $DATABRICKS_API_TOKEN
+echo $DATABRICKS_TOKEN
 echo $DATABRICKS_ORDGID
 echo $DATABRICKS_CLUSTER_ID
 
@@ -8,8 +8,8 @@ pip install databricks-connect
 
 databricks-connect configure <<EOF
 y 
-https://adb-1330140498858410.10.azuredatabricks.net
-$DATABRICKS_API_TOKEN
+'"$DATABRICKS_HOST"'
+$DATABRICKS_TOKEN
 $DATABRICKS_CLUSTER_ID
 $DATABRICKS_ORDGID
 15001
@@ -17,11 +17,11 @@ EOF
 
 databricks-connect test
 
-#echo "y
-#'"$DATABRICKS_HOST"'
-#$DATABRICKS_API_TOKEN
-#$DATABRICKS_CLUSTER_ID
-#$DATABRICKS_ORDGID
-#15001" | databricks-connect configure
+echo "y
+'"$DATABRICKS_HOST"'
+$DATABRICKS_TOKEN
+$DATABRICKS_CLUSTER_ID
+$DATABRICKS_ORDGID
+15001" | databricks-connect configure
 
-#databricks-connect test
+databricks-connect test
