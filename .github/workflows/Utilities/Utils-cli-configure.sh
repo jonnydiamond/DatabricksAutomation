28ee0a9b0a3c2bd299ed963ab9c730d1b39e9867
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-27 14:46:05 +0100

    created databricks connect module pip

--- .github/workflows/Utilities/Utils-cli-configure.sh
+++ .github/workflows/Utilities/Utils-cli-configure.sh
@@ -3,26 +3,26 @@ echo $DATABRICKS_TOKEN
 echo $DATABRICKS_ORDGID
 echo $DATABRICKS_CLUSTER_ID
 
-pip install -U databricks-connect
+pip install databricks-connect
 
 
 databricks-connect configure <<EOF
 y 
-"$DATABRICKS_HOST"
-"$DATABRICKS_TOKEN"
-"$DATABRICKS_CLUSTER_ID"
-"$DATABRICKS_ORDGID"
+$DATABRICKS_HOST
+$DATABRICKS_TOKEN
+$DATABRICKS_CLUSTER_ID
+$DATABRICKS_ORDGID
 15001
 EOF
 
 databricks-connect test
 
-#databricks-connect test
+echo "Second Attempt"
+echo "y
+'"$DATABRICKS_HOST"'
+$DATABRICKS_TOKEN
+$DATABRICKS_CLUSTER_ID
+$DATABRICKS_ORDGID
+15001" | databricks-connect configure
 
-#echo "Second Attempt"
-#echo "y
-#'"$DATABRICKS_HOST"'
-#$DATABRICKS_TOKEN
-#$DATABRICKS_CLUSTER_ID
-#$DATABRICKS_ORDGID
-#15001" | databricks-connect configure
+databricks-connect test
