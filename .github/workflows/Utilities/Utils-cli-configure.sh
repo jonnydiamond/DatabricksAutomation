b3562fe7c08ace43a9c66bdc6a54e8e38038313e
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-07-27 15:22:06 +0100

    created databricks connect module pip

--- .github/workflows/Utilities/Utils-cli-configure.sh
+++ .github/workflows/Utilities/Utils-cli-configure.sh
@@ -2,22 +2,28 @@ echo $DATABRICKS_HOST
 echo $DATABRICKS_TOKEN
 echo $DATABRICKS_ORDGID
 echo $DATABRICKS_CLUSTER_ID
+pip install databricks-cli --upgrade
+#pip uninstall pyspark
+#pip install -U databricks-connect=="10.4.0b0"
+#apt install openjdk-8-jdk
 
-pip uninstall pyspark
-pip install -U databricks-connect=="10.4.0b0"
-apt install openjdk-8-jdk
 
-
-databricks-connect configure <<EOF
-y 
-"$DATABRICKS_HOST"
+databricks configure --token << EOF
+$DATABRICKS_HOST
 $DATABRICKS_TOKEN
-$DATABRICKS_CLUSTER_ID
-$DATABRICKS_ORDGID
-15001
 EOF
-databricks-connect configure
-databricks-connect test
+
+
+#databricks-connect configure <<EOF
+#y 
+#"$DATABRICKS_HOST"
+#$DATABRICKS_TOKEN
+#$DATABRICKS_CLUSTER_ID
+#$DATABRICKS_ORDGID
+#15001
+#EOF
+#databricks-connect configure
+#databricks-connect test
 
 
 #echo $DATABRICKS_HOST
