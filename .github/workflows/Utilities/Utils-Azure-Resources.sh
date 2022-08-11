2b64dd32d8dd9f6ff396aeb37c68c424bd7f5eb2
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-08-11 17:30:36 +0000

    Removed Folders In workflows

--- .github/workflows/Utilities/Utils-Azure-Resources.sh
+++ .github/workflows/Utilities/Utils-Azure-Resources.sh
@@ -2,7 +2,6 @@ echo "environment variable: $param_TemplateParamFilePath"
 echo "environment variable: $param_Location"
 echo "environment variable: $param_TemplateFilePath"
 # Important to define unique deployment names as conflicts will occur
-
 echo "Create Azure DBX Resource Environments...."
 az deployment sub create \
 --location $param_Location \
