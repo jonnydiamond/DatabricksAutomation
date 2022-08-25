37a78e4fa504f808a5294adbdb2c843a49ab1d9f
Ciaran Hamill Diamond <ciaranh@microsoft.com>
2022-08-25 17:53:51 +0100

    test

--- .github/workflows/Utilities/Utils-Assign-RBAC.sh
+++ .github/workflows/Utilities/Utils-Assign-RBAC.sh
@@ -21,9 +21,9 @@ for row in $(echo "${json}" | jq -r '.RBAC_Assignments[] | @base64'); do
     }
     ROLES_ARRAY=$(_jq '.roles')
     echo $ROLES_ARRAY
-    KEYS=($(( echo "$ROLES_ARRAY" | jq -r '@sh')| tr -d \'\"))
-    #echo "Array size: " ${#KEYS[@]}
-    #echo "Array elements: "${KEYS[@]}
+    KEYS=($(( echo "$ROLES_ARRAY" | jq -r '@sh')))
+    echo "Array size: " ${#KEYS[@]}
+    echo "Array elements: "${KEYS[@]}
     for ROLE in "${KEYS[@]}"; do
         echo $ROLE 
     done
